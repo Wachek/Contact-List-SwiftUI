082266e6505abed868472b619fe1e31d70b9d4c8
Wachek <wachek7@gmail.com>
2022-10-26 21:41:53 +0300

    setup data model

---
+++ Contact List SwiftUI/Models/Person.swift
@@ -0,0 +1,46 @@
+//
+//  Person.swift
+//  Contact List SwiftUI
+//
+//  Created by Вячеслав Турчак on 26.10.2022.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let lastname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(lastname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let lastnames = DataManager.shared.lastnames.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        let iterationCount = min(names.count, lastnames.count, emails.count, phones.count)
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                lastname: lastnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
